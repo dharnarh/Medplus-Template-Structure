9ce59ec23f2eef126f5b22e9788bf75d92593488
Umar Farouq Mohammed <upharouk@gmail.com>
2020-02-25 18:39:40 +0000

    Stores template

--- stores.php
+++ stores.php
@@ -3,75 +3,35 @@
 <!-- You need this element to prevent the content of the page from jumping up -->
 <div class="header-fixed-placeholder"></div>
 
-<?php include 'includes/header.php' ?>
-
-<!-- You need this element to prevent the content of the page from jumping up -->
-<div class="header-fixed-placeholder"></div>
-
 <div class="container-fluid pd">
   <div class="row">
     <div class="col-lg-12">
-      <h1 class="text-body text-center font-secondary mb-5 t-w200" style="text-transform: uppercase;">CONTENT <span class="font-weight-bold">PAGE</span></h1>
+      <h1 class="text-body text-center font-secondary mb-5 t-w200" style="text-transform: uppercase;">OUR <span class="font-weight-bold">STORES</span></h1>
     </div>
   </div>
   <div class="row">
     <div class="col-md-12">
-      <img src="images/about-bg.png" style="width: 100%;">
-    </div>
-    <div class="col-md-12 about-text">
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-      <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-      <p>It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-      <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
-    </div>
-  </div>
-</div>
-
-<div class="container-fluid pd" style="background: #f1f1f1;">
-  <div class="row">
-    <div class="col-md-12">
-      <h1 class="text-body text-center font-secondary t-w200" style="text-transform: uppercase;">OUR <span class="font-weight-bold">MISSION</span></h1>
-    </div>
-    <div class="col-md-12 about-text2">
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-      <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </p>
-    </div>
-  </div>
-</div>
-<div class="container-fluid pd">
-  <div class="row">
-    <div class="col-md-4 text-center scn">
-      <img src="images/icons/q1.png" width="150px" class="mb-2">
-      <h5 class="mb-3">QUALITY</h5>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.</p>
-    </div>
-    <div class="col-md-4 text-center scn">
-      <img src="images/icons/cm1.png" width="150px" class="mb-2">
-      <h5 class="mb-3">COMFORT</h5>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.</p>
-    </div>
-    <div class="col-md-4 text-center scn">
-      <img src="images/icons/e1.png" width="150px" class="mb-2">
-      <h5 class="mb-3">EXPERIENCE</h5>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.</p>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1145.1346257475343!2d3.412666885496601!3d6.442365619052318!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8b2910399779%3A0x74df32c4aca0f891!2sMedplus%20Pharmacy!5e0!3m2!1sen!2sgh!4v1582655128319!5m2!1sen!2sgh" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+      <ul class="list-group list-group-flush py-3">
+          <li class="list-group-item">
+            <p class="h4 font-weight-bold">38 Awolowo Rd, Ikoyi, Lagos, Nigeria</p>
+            <a href="tel:++2347013489468"><p>+234 701 348 9468</p></a>
+          </li>
+          <li class="list-group-item">
+            <p class="h4 font-weight-bold">1004 Housing Estate, Ozumba Mbadiwe Rd, Eti-Osa, Lagos, Nigeria</p>
+            <a href="tel:+2348027272010"><p>+234 802 727 2010</p></a>
+          </li>
+          <li class="list-group-item">
+            <p class="h4 font-weight-bold">38 Awolowo Rd, Ikoyi, Lagos, Nigeria</p>
+            <a href="tel:+2347013489468"><p>+234 701 348 9468</p></a>
+          </li>
+          <li class="list-group-item">
+            <p class="h4 font-weight-bold">1004 Housing Estate, Ozumba Mbadiwe Rd, Eti-Osa, Lagos, Nigeria</p>
+            <a href="tel:+2348027272010"><p>+234 802 727 2010</p></a>
+          </li>
+        </ul>
     </div>
   </div>
 </div>
-<div class="container-fluid pd">
-  <div class="row">
-    <div class="col-md-12 text-center scn">
-      <img src="images/g1.png" width="100%" class="mb-2">
-    </div>
-    <div class="col-md-6 text-center scn">
-      <img src="images/g2.png" width="100%" class="mb-2">
-    </div>
-    <div class="col-md-6 text-center scn">
-      <p> <img src="images/g3.png" width="100%" class="mb-2"></p>
-      <p> <img src="images/g4.png" width="100%" class="mb-2"></p>
-    </div>
-  </div>
-</div>
-
-<?php include 'includes/footer.php' ?>
 
 <?php include 'includes/footer.php' ?>
